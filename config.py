180b4df60ca5a29a298b65cfc97b11fa93ea8724
joshi2000156-hue <joshi2000156@gmail.com>
2026-03-31 21:15:44 +0530

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-API_KEY = "AIzaSyB64ON5X-puENIkHDcLGnSoNOQfEX1f9rc"
-
-SEARCH_URL = "https://www.googleapis.com/youtube/v3/search"
-VIDEO_URL = "https://www.googleapis.com/youtube/v3/videos"
-
-MAX_RESULTS = 100
